daa917645efe7c30dd739f1f77d9fe32a5029e78
Gabriel Cabral Toñanez <cabraltonanez@gmail.com>
2023-07-04 07:51:15 -0300

    primeros pasos bookApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [books, setBooks] = useState([]);
 
+	useEffect(() => {
+		axios
+			.get(`http://localhost:8080/books/all`)
+			.then((res) => {
+				setBooks(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
+
+	const eliminarLibros = () => {
+		axios
+			.delete(`http://localhost:8080/books/deleteAll`)
+			.then((res) => {
+				console.log(res.data);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	return (
+		<div className='header' style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
+			<div style={{display: 'flex', justifyContent: 'center'}}>
+				<h1>LibrosAPP</h1>
+				<button onClick={eliminarLibros}>Eliminar todos los libros</button>
+			</div>
+
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					gap: '25px',
+					flexWrap: 'wrap',
+				}}>
+				{books.length > 0
+					? books.map((book) => (
+							<div style={{display: 'flex', flexDirection: 'column', gap: '15px'}}>
+								<h4>
+									Título: {book.title}
+									<br />
+									Autor: {book.author}
+									<br />
+									Precio: ${book.price}
+									<br />
+									Fecha de lanzamiento: {book.releaseDate}
+									<br />
+								</h4>
+								<div style={{display: 'flex', justifyContent: 'space-around'}}>
+									<button onClick={() => console.log('Boton Editar')}>Editar</button>
+									<button onClick={() => console.log('Boton Eliminar')}>Eliminar</button>
+								</div>
+							</div>
+					  ))
+					: 'No hay libros cargados en la base'}
+			</div>
+		</div>
+	);
+};
 export default App;
